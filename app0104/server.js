201be9140757aab516370f0cadcb13ffe8a3b5b8
potato <ymcho.dev@gmail.com>
2023-01-04 18:02:18 +0900

    20230104 node.js & db connect study

--- app0104/server.js
+++ app0104/server.js
@@ -25,22 +25,49 @@ let server = http.createServer(function (request, response) {
                 "database": "jsp"
             };
             let con = mysql.createConnection(constr); // 접속객체
+            // 트랜잭션 시작
+            con.beginTransaction(); // 트랜잭션 시작
             let sql = "insert into member(id, pass, mail_receive)";
             sql += " values(?, ?, ?)";
             con.query(sql, [json.id, json.pass, json.mail_receive], function (err, result) {
                 // 쿼리 수행, 익명함수를 통해 수행여부 확인 
                 if (err) {
                     response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
-                    response.end(등록실패);
+                    response.end("등록실패");
                 } else {
-                    response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
-                    response.end(등록성공);
+                    // response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+                    // response.end("등록성공");
+                    // con.rollback(); // con.commit();
+                    // 가장 최근 나로 인해 추가된 PK 가져오기
+                    sql = "select last_insert_id() as member_idx";
+                    con.query(sql, function (err2, rs, fields) { // 바인드 변수가 없으므로 안가져와도 됨
+                        if (err2) {
+                            console.log("에러", err2);
+                        } else {
+                            console.log("방금 생성된 IDX는 ", rs[0].member_idx);
+                            // 취미레코드 넣기
+                            for (let i = 0; i < json.hobby_name.length; i++) {
+                                sql= "insert into hobby(member_idx, hobby_name)";
+                                sql += " values(?, ?)";
+                                con.query(sql, [rs[0].member_idx, json.hobby_name[i]], function(err3, result3) {
+                                    if(err3) {
+                                        console.log("취미등록실패", err3);
+                                        con.rollback();
+                                    } else {
+                                        response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+                                        response.end("등록성공");
+                                        con.commit(); // con.rollback(); // con.commit();
+                                    }
+                                    // con.end();
+                                });
+                            }
+                        }
+                    });
                 }
-                con.end();
             });
         });
-        response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
-        response.end("원하니 등록");
+        // response.writeHead(200, { "Content-Type": "text/html;charset=UTF-8" });
+        // response.end("원하니 등록");
     }
 });
 server.listen(7777, function () {
